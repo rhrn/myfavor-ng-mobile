25b553b31a2c65c885c431514bd3efbab7c5f38a
Nesterov Roman <nester@bk.ru>
2017-09-29 16:54:40 +0300

    Migrate to nativescript 3.1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { NativeScriptHttpModule } from 'nativescript-angular/http';
 
 import { AppComponent } from "./app.component";
